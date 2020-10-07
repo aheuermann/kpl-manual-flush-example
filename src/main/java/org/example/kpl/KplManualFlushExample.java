bf26c9b5cc05752e2b8514a90c4fb1cb805da289
Andrew Heuermann <aheuermann@gmail.com>
2020-10-07 15:09:14 -0500

    Initial commit

--- src/main/java/org/example/kpl/KplManualFlushExample.java
+++ src/main/java/org/example/kpl/KplManualFlushExample.java
@@ -29,8 +29,8 @@ public class KplManualFlushExample {
             long startTime = System.currentTimeMillis();
 
             List<Message> msgs = new ArrayList<>();
-            LOGGER.info("Generating batch of user records [batchNumber={}, size={}]", ++batchCount, batchSize);
-            for (int i = 0; i <= batchSize; ++i) {
+            LOGGER.info("Generating batch of user records [batchNumber={}, size={}]", batchCount, batchSize);
+            for (int i = 0; i < batchSize; ++i) {
                 ++sequenceNumber;
                 msgs.add(new Message(sequenceNumber, generateData(sequenceNumber, messageLength)));
             }
@@ -40,14 +40,14 @@ public class KplManualFlushExample {
             for (Message msg : msgs) {
                 futures.add(producer.addUserRecord(stream, msg.partitionKey, msg.bytes));
             }
-            LOGGER.info("Done adding user records, flushing [batchNumber={}, size={}]", ++batchCount, futures.size());
+            LOGGER.info("Done adding user records, flushing [batchNumber={}, size={}]", batchCount, futures.size());
             // Call flush to force publish of messages.
             // Expected this to trigger all messages to be published, but some messages are still remain in the bugger and don't get published until the RecordMaxBufferedTime is met.
             producer.flush();
             try {
                 ListenableFuture<List<UserRecordResult>> allFutures = Futures.allAsList(futures);
                 allFutures.get();
-                LOGGER.info("Done publishing batch [batchNumber={}, size={}, durationMs={}]", ++batchCount, futures.size(), System.currentTimeMillis() - startTime);
+                LOGGER.info("Done publishing batch [batchNumber={}, size={}, duration={}ms]", batchCount, futures.size(), System.currentTimeMillis() - startTime);
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
                 break;
@@ -84,7 +84,7 @@ public class KplManualFlushExample {
                 .setRegion("us-west-2")
                 .setAggregationEnabled(true)
                 .setVerifyCertificate(false)
-                .setRecordMaxBufferedTime(60000L)
+                .setRecordMaxBufferedTime(10000L)
                 .setRecordTtl(Integer.MAX_VALUE); // set to max value so we don't discard records if they fail to publish to kinesis
         return new KinesisProducer(config);
     }
